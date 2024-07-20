816a93bbeb713dc1243f69657cade541d624c012
Hojung Kim <mireu9275@naver.com>
2024-07-20 15:14:18 +0900

    feature: gradle 개발환경 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("maven-publish")
 }
 
 group = "kr.eme.semiTerminal"
@@ -7,9 +8,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.dmulloy2.net/repository/public/") //ProtocolLib
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnly("com.comphenix.protocol:ProtocolLib:5.2.0-SNAPSHOT")
+//    compileOnly("org.spigotmc:spigot:1.20.2-R0.1-SNAPSHOT")
     testImplementation(kotlin("test"))
 }
 
@@ -18,4 +25,23 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+tasks.jar {
+    archiveFileName = "${project.name}-${project.version}.jar"
+    destinationDirectory = file("d:\\minecraft\\1. 버킷 관련\\1.20.2 paper_dev2\\plugins")
+    manifest {
+        attributes["main-class"] = "kr.eme.semiTerminal.SemiTerminal"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "kr.eme.semiTerminal"
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["kotlin"])
+        }
+    }
 }
