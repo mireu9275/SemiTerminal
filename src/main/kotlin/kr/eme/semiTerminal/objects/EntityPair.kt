e958638896e448343ac924e0e815acde917f097d
Hojung Kim <mireu9275@naver.com>
2024-07-20 15:36:40 +0900

    feature: DataClass EntityPair 추가

---
+++ src/main/kotlin/kr/eme/semiTerminal/objects/EntityPair.kt
@@ -0,0 +1,10 @@
+package kr.eme.semiTerminal.objects
+
+import org.bukkit.entity.ArmorStand
+import org.bukkit.entity.TextDisplay
+
+data class EntityPair(
+    val armorStand: ArmorStand,
+    val textDisplay: TextDisplay
+)
+
