108b9ebaa7923b843fea8be469dda36b84674d72
Hojung Kim <mireu9275@naver.com>
2024-07-20 16:42:02 +0900

    feature: 메인클래스에서 패킷매니저의 생성자 함수를 실행하도록 함

--- src/main/kotlin/kr/eme/semiTerminal/SemiTerminal.kt
+++ src/main/kotlin/kr/eme/semiTerminal/SemiTerminal.kt
@@ -2,6 +2,7 @@ package kr.eme.semiTerminal
 
 import kr.eme.semiTerminal.commands.TerminalCommand
 import kr.eme.semiTerminal.listeners.TerminalListener
+import kr.eme.semiTerminal.managers.PacketManager
 import org.bukkit.plugin.java.JavaPlugin
 
 
@@ -9,6 +10,8 @@ internal lateinit var main : JavaPlugin
 
 class SemiTerminal : JavaPlugin() {
     override fun onEnable() {
+        main = this
+        PacketManager.init()
         server.pluginManager.registerEvents(TerminalListener,this)
         getCommand("terminal")?.setExecutor(TerminalCommand)
     }
