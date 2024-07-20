739198b3bd9f53d8f97cfd5be66f71f0915ac1b2
Hojung Kim <mireu9275@naver.com>
2024-07-20 15:34:53 +0900

    update: 리스너, 이벤트 등록

--- src/main/kotlin/kr/eme/semiTerminal/SemiTerminal.kt
+++ src/main/kotlin/kr/eme/semiTerminal/SemiTerminal.kt
@@ -1,10 +1,16 @@
 package kr.eme.semiTerminal
 
+import kr.eme.semiTerminal.commands.TerminalCommand
+import kr.eme.semiTerminal.listeners.TerminalListener
 import org.bukkit.plugin.java.JavaPlugin
+
+
+internal lateinit var main : JavaPlugin
 
 class SemiTerminal : JavaPlugin() {
     override fun onEnable() {
-        super.onEnable()
+        server.pluginManager.registerEvents(TerminalListener,this)
+        getCommand("terminal")?.setExecutor(TerminalCommand)
     }
     override fun onDisable() {
         super.onDisable()
