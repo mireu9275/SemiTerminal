bc65ae5b6097819640e088bb8353432e71e6ea10
Hojung Kim <mireu9275@naver.com>
2024-07-20 15:29:21 +0900

    feature: 컴포넌트 유틸에 문자열 형식 변환 함수 추가

--- src/main/kotlin/kr/eme/semiTerminal/utils/ComponentUtils.kt
+++ src/main/kotlin/kr/eme/semiTerminal/utils/ComponentUtils.kt
@@ -1,4 +1,12 @@
 package kr.eme.semiTerminal.utils
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
+
 object ComponentUtils {
+    private val plainSerializer = PlainTextComponentSerializer.plainText()
+
+    fun convertComponentToString(component: Component?): String {
+        return component?.let { plainSerializer.serialize(it) } ?: ""
+    }
 }
