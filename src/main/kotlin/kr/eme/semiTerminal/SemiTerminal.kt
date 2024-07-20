5e906ebf08a5980fe97205c765fa8019d5665572
Hojung Kim <mireu9275@naver.com>
2024-07-20 15:16:40 +0900

    feature: Main Class SemiTerminal 추가

---
+++ src/main/kotlin/kr/eme/semiTerminal/SemiTerminal.kt
@@ -0,0 +1,12 @@
+package kr.eme.semiTerminal
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SemiTerminal : JavaPlugin() {
+    override fun onEnable() {
+        super.onEnable()
+    }
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
