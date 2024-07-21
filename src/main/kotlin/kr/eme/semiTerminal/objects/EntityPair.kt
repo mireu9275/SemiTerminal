0fecbb6907cbb4011cf43e6f2c3c07e0baaa1ab2
Hojung Kim <mireu9275@naver.com>
2024-07-21 21:02:28 +0900

    feature: EntityPair 변수 추가
    - 선택한 메뉴 확인 변수 추가
    - 현재 화면 정보 변수 추가

--- src/main/kotlin/kr/eme/semiTerminal/objects/EntityPair.kt
+++ src/main/kotlin/kr/eme/semiTerminal/objects/EntityPair.kt
@@ -5,6 +5,8 @@ import org.bukkit.entity.TextDisplay
 
 data class EntityPair(
     val armorStand: ArmorStand,
-    val textDisplay: TextDisplay
+    val textDisplay: TextDisplay,
+    var selectedOption : Int = 0, // 0: 네, 1: 아니요
+    var sceneIndex: Int = 0
 )
 
