bbf797b08e9b63bd9cd1fdbd7d4935cdcc86537e
Hojung Kim <mireu9275@naver.com>
2024-07-21 22:41:09 +0900

    feature: Scene 설정 기능 및 메뉴 변경 기능 추가

--- src/main/kotlin/kr/eme/semiTerminal/listeners/TerminalListener.kt
+++ src/main/kotlin/kr/eme/semiTerminal/listeners/TerminalListener.kt
@@ -1,6 +1,12 @@
 package kr.eme.semiTerminal.listeners
 
+import kr.eme.semiTerminal.enums.Scene
+import kr.eme.semiTerminal.managers.EntityPairManager.getEntityPair
+import kr.eme.semiTerminal.managers.EntityPairManager.removeEntityPair
+import kr.eme.semiTerminal.managers.EntityPairManager.updateTextDisplay
 import kr.eme.semiTerminal.managers.PacketManager.mutePlayer
+import kr.eme.semiTerminal.objects.EntityPair
+import net.kyori.adventure.text.Component
 import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -14,10 +20,44 @@ object TerminalListener : Listener {
         val entity = event.rightClicked
         val player = event.player
         if (entity !is ArmorStand) return
+
+        val key = entity.customName() ?: return
+        val entityPair = getEntityPair(key) ?: return
         // 플레이어가 웅크린 후 우클릭을 한다면 확정함.
-        if (player.isSneaking) player.sendMessage("Shift + Right Click")
+        if (player.isSneaking) {
+            player.sendMessage("Shift + Right Click")
+
+            val selectedOption = entityPair.selectedOption
+            val sceneIndex = Scene.fromInt(entityPair.sceneIndex)
+            when (sceneIndex) {
+                Scene.MAIN -> {
+                    when (selectedOption) {
+                        0 -> { // YES 클릭
+                            NextDisplay(entityPair, 1)
+                            entityPair.textDisplay.text(Component.text("선택해주세요.\n§a[작물]§f[기타]"))
+                        }
+                        1 -> { // NO 클릭
+                            removeEntityPair(key)
+                        }
+                    }
+                }
+                Scene.SHOP_SELECT -> {
+                    when (selectedOption) {
+                        0 -> { // 작물 클릭
+                            NextDisplay(entityPair, 2)
+                            entityPair.textDisplay.text(Component.text("구매할 작물을 선택해주세요.\n§a[봄]§f[여름][가을]"))
+                        }
+                    }
+                }
+                else -> return
+            }
+        }
         // 플레이어가 웅크리지 않고 우클릭을 한다면 메뉴를 오른쪽으로 옮김.
-        else player.sendMessage("Right Click")
+        else {
+            player.sendMessage("Right Click")
+            entityPair.selectedOption = (entityPair.selectedOption + 1) % 2
+            updateTextDisplay(entityPair)
+        }
     }
     @EventHandler
     fun onPlayerDamageEntity(event: EntityDamageByEntityEvent){
@@ -27,6 +67,36 @@ object TerminalListener : Listener {
             event.isCancelled = true
             mutePlayer(damager)
             damager.sendMessage("Left Click")
+
+            val key = entity.customName() ?: return
+            val entityPair = getEntityPair(key) ?: return
+
+            entityPair.selectedOption = (entityPair.selectedOption + 1) % 2
+            updateTextDisplay(entityPair)
         }
     }
+
+    /**
+     * Next display
+     *
+     * 다음 디스플레이로 넘어갈 떄, EntityPair 의 화면 번호를 증가시키고 선택한 메뉴를 0으로 설정함.
+     *
+     * @param entityPair
+     */
+    private fun NextDisplay(entityPair: EntityPair, sceneIndex: Int) {
+        entityPair.sceneIndex = sceneIndex
+        entityPair.selectedOption = 0
+    }
+    /**
+     * Previous display
+     *
+     * 이전 디스플레이로 넘어갈 때, EntityPair 의 화면 번호를 감소시키고 선택한 메뉴를 0으로 설정함.
+     *
+     * @param entityPair
+     */
+    private fun PreviousDisplay(entityPair: EntityPair, sceneIndex: Int) {
+        entityPair.sceneIndex = sceneIndex
+        entityPair.selectedOption = 0
+    }
 }
+
